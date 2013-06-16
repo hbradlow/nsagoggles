4306941279906053dedf315136b98d8b5c008557
Victor Zhang <zhang.victorz@gmail.com>
2013-06-16 03:27:03 -0700

    weee

--- goggles.js
+++ goggles.js
@@ -31,12 +31,38 @@ setInterval(function(){
 setTimeout(function(){
     var num_words = 0;
     var text = jQuery('html').text().toLowerCase();
-    console.log(text);
+
+    
+
+    //Fun Suicide Function (lol)
+
     jQuery.each(blacklist,function(index,item){ 
         if(text.indexOf(item.toLowerCase())!=-1){
             console.log(item);
             num_words += 1;
         }
+
     });
     console.log(num_words + " terror words");
 }, 2000);
+
+window.onload=function(){
+ 
+   jQuery('<div/>', {
+        id: 'foo',
+        position: 'absolute',
+        height: "1000px",
+        width: "100%"
+    }).appendTo(document.body);
+
+   jQuery('foo').css('background-color','red');
+        //jQuery('body').css("display", "none");  
+        //jQuery('body').fadeTo(500,0.5,function(){});
+
+        /*jQuery('body').wrapInner("<div id='dimmer'></div>");
+        jQuery('#dimmer').hide().css({
+            "height": "100%",
+            "width": "100%",
+            "background-color": "red"
+        }).fadeTo(2000, 0.8);*/
+}
