8663099c4be5f4108d7d6a4e0bedb7b88c265000
Henry Bradlow <hbradlow@berkeley.edu>
2013-06-15 19:11:26 -0700

    Added blacklist parsing

---
+++ blacklist_parser.py
@@ -0,0 +1,17 @@
+blacklist = []
+with open("blacklist_newline.txt") as f:
+    try:
+        item = f.next()
+        while item:
+            blacklist.append(item.strip())
+            item = f.next()
+    except StopIteration:
+        pass
+with open("blacklist_comma.txt") as f:
+    data = f.read()
+    for item in data.split(","):
+        blacklist.append(item.strip())
+with open("blacklist.js","w") as f:
+    f.write("blacklist = ")
+    f.write(str(blacklist))
+    f.write("\n")
